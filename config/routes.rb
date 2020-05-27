b6d189438a312de2a70b11756c9f533cfff99d62
Freddy Munive <munivef@yahoo.com>
2020-05-27 16:57:29 -0500

    adding new and create actions to games and companies

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  # get 'companies/index'
-  # get 'companies/show'
-  # get 'games/index'
-  # # get 'games/show'
-  # get 'games/:id', to: 'games#show'
-  resources :companies, only: [:index, :show]
-  resources :games, only: [:index, :show]
+  resources :companies, only: [:index, :show, :new, :create, :edit, :update]
+  resources :games, only: [:index, :show, :new, :create, :edit, :update]
 end
